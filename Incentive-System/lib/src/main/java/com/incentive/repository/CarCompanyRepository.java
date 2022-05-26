4ae7d7deda901e720cfd3e63a27947d70e5e9dee
41Shahid <103582173+41Shahid@users.noreply.github.com>
2022-05-26 17:45:07 +0530

    CarCompanyRepository

---
+++ Incentive-System/lib/src/main/java/com/incentive/repository/CarCompanyRepository.java
@@ -0,0 +1,9 @@
+package com.incentive.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.incentive.entities.CarCompany;
+
+public interface CarCompanyRepository extends JpaRepository<CarCompany, Integer>{
+
+}
