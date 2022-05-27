267e3f102c7ab5a74e3da8fb7a8ecbad54e8d8bc
41Shahid <103582173+41Shahid@users.noreply.github.com>
2022-05-27 08:40:03 +0530

    Updated5

--- Incentive-System/lib/src/main/java/com/incentive/service/IncentiveDetailsService.java
+++ Incentive-System/lib/src/main/java/com/incentive/service/IncentiveDetailsService.java
@@ -1,5 +1,10 @@
 package com.incentive.service;
 
+import java.util.List;
+
+import com.incentive.entities.IncentiveDetails;
+
 public interface IncentiveDetailsService {
+	List<IncentiveDetails> viewIncentiveDetails();
 
 }
